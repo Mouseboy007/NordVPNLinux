f90c20ba216cbed5ec06c4d33b322fb5de00d522
Mouseboy007 <66361777+Mouseboy007@users.noreply.github.com>
2024-08-02 10:28:50 +0100

    Add files via upload

--- boot_time.sh
+++ boot_time.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-VPNDir='<<YOUR_PATH_HERE>>' # example '/home/bob/mystuff/vpnfolder'
+VPNDir='/home/docker/shared/vpn'
 
 rm -f /tmp/vpnlock
 sudo chown -R +777 $VPNDir/*
 sudo chmod -R +777 $VPNDir/*
 sudo chmod -R +X $VPNDir/*.sh
 sudo -u docker nordvpn set technology nordlynx
+sudo $VPNDir/get_vpn_best_server.sh
 sudo $VPNDir/set_vpn_best_server.sh
 sudo $VPNDir/set_vpn_iptables.sh
 
